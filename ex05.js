2743af69b6729577ff68fa09599f9c69259009c9
LogoMan1995 <david.grigorian777@gmail.com>
2024-04-09 11:38:18 +0400

    Sorted Union

---
+++ ex05.js
@@ -0,0 +1,18 @@
+// 5. Сортированный союз.
+
+
+
+function uniteUnique(arr) {
+    const args = [...arguments];
+    const result = [];
+    for (let i = 0; i < args.length; i++) {
+      for (let j = 0; j < args[i].length; j++) {
+        if (!result.includes(args[i][j])) {
+          result.push(args[i][j]);
+        }
+      }
+    }
+    return result;
+  }
+  
+  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
